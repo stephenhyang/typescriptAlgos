934ccfd05deacbdcfe4bebbfdb89ed9b223ac711
Stephen Yang <stephen.yang98@gmail.com>
2024-10-07 19:53:50 -0700

    BFS

--- kata-machine/src/day1/BTBFS.ts
+++ kata-machine/src/day1/BTBFS.ts
@@ -1,3 +1,25 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+  const q: (BinaryNode<number> | null)[] = [head];
+
+  while (q.length) {
+
+    const curr = q.shift() as BinaryNode<number> | undefined | null;
+    if (!curr){
+      continue;
+    }
+
+    if (curr.value === needle){
+      return true;
+    }
+
+    if (curr.left){
+      q.push(curr.left);
+    }
+    if (curr.right){
+      q.push(curr.right);
+    }
+  }
+
+  return false;
 }
